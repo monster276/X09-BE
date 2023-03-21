d6371087d15d2275278f705b17492f1deebb1c87
trunghieu <trunghieu0451@gmail.com>
2023-03-21 15:33:05 +0700

    feat: :sparkles: validator location

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 const {
   getLocations,
@@ -11,8 +12,48 @@ const {
 
 router.get("/", getLocations);
 router.get("/:id", getLocationById);
-router.post("/", createLocation);
+router.post(
+  "/",
+  body("id", "ID is string and ID is required, ID length must be less than 5")
+    .isLength({ max: 5 })
+    .isString()
+    .not()
+    .isEmpty(),
+  body("name", "Name is string and Name is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("address", "Address is string and Address is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("status", "Status is boolean and Status is required")
+    .isBoolean()
+    .not()
+    .isEmpty(),
+  createLocation
+);
 router.delete("/:id", deleteLocation);
-router.put("/:id", updateLocation);
+router.put(
+  "/:id",
+  body("id", "ID is string and ID is required, ID length must be less than 5")
+    .isLength({ max: 5 })
+    .isString()
+    .not()
+    .isEmpty(),
+  body("name", "Name is string and Name is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("address", "Address is string and Address is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("status", "Status is boolean and Status is required")
+    .isBoolean()
+    .not()
+    .isEmpty(),
+  updateLocation
+);
 
 module.exports = router;
