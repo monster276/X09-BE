7c8301c5a2a479985c329756d13ce98e2937b305
trunghieu <trunghieu0451@gmail.com>
2023-03-21 16:47:18 +0700

    feat: :sparkles: add classroom, course validator

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -49,6 +49,41 @@ router.post(
   createCourse
 );
 router.delete("/:id", deleteCourse);
-router.put("/:id", updateCourse);
+router.put(
+  "/:id",
+  body("id", "ID is string and ID is required").isString().not().isEmpty(),
+  body("name", "Name is string and Name is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("description", "Description is string and Description is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("image", "Image is string and Image is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("courseTime", "Course Time is string and Course Time is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("classTime", "Class Time is string and Class Time is required")
+    .isString()
+    .not()
+    .isEmpty(),
+  body("price", "Price is number and Price is required")
+    .isNumeric()
+    .not()
+    .isEmpty(),
+  body(
+    "maxNumberOfStudents",
+    "Max Number Of Students is number and Max Number Of Students is required"
+  )
+    .isNumeric()
+    .not()
+    .isEmpty(),
+  updateCourse
+);
 
 module.exports = router;
