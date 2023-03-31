5c34b8ba153b43e1669043ce2c0d0d2d5f60e8f3
trunghieu <trunghieu0451@gmail.com>
2023-03-31 19:15:37 +0700

    fix merge

--- index.js
+++ index.js
@@ -1,65 +1,42 @@
-require('dotenv').config()
-const path = require('path')
-const cors = require('cors')
-const express = require('express')
-const { connectToDB } = require('./config/db')
+require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+const { connectToDB } = require("./config/db");
 
-// Routes
-<<<<<<< HEAD
-const authRoutes = require('./routes/auth')
-const courseRoutes = require('./routes/courseRoutes')
-const classroomRoutes = require('./routes/classroomRoutes')
-const uploadRoutes = require('./routes/uploadRoutes')
-const locationRoutes = require('./routes/locationRoutes')
-const lectureRoutes = require('./routes/lectureRoutes')
-const enrollCourseRoutes = require('./routes/enrollCourseRoutes')
-const userRoute = require("./routes/userRoute")
-=======
 const authRoutes = require("./routes/auth");
 const courseRoutes = require("./routes/courseRoutes");
 const classroomRoutes = require("./routes/classroomRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 const locationRoutes = require("./routes/locationRoutes");
 const lectureRoutes = require("./routes/lectureRoutes");
-const lessonRoutes = require("./routes/lessonRoutes");
-const studentRoutes = require("./routes/studentRoutes");
+const enrollCourseRoutes = require("./routes/enrollCourseRoutes");
+const userRoute = require("./routes/userRoute");
 
->>>>>>> 502615175cff8189f6f5f2ebf8082326865acce4
 //Connect Mongo
-connectToDB()
+connectToDB();
 
-const app = express()
+const app = express();
 
-app.use(cors('*'))
-app.use(express.json())
+app.use(cors("*"));
+app.use(express.json());
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the API' }))
+app.get("/", (req, res) => res.json({ msg: "Welcome to the API" }));
 
 // Define Routes
-<<<<<<< HEAD
-app.use('/api/courses', courseRoutes)
-app.use('/api/classrooms', classroomRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoute)
-app.use('/api/upload', uploadRoutes)
-app.use('/api/locations', locationRoutes)
-app.use('/api/lectures', lectureRoutes)
-app.use('/api/enrollCourse', enrollCourseRoutes)
-const dirname = path.resolve()
-app.use('/uploads', express.static(path.join(dirname, '/uploads')))
-=======
 app.use("/api/courses", courseRoutes);
 app.use("/api/classrooms", classroomRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoute);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/locations", locationRoutes);
 app.use("/api/lectures", lectureRoutes);
-app.use("/api/lessons", lessonRoutes);
-app.use("/api/students", studentRoutes);
->>>>>>> 502615175cff8189f6f5f2ebf8082326865acce4
+app.use("/api/enrollCourse", enrollCourseRoutes);
+const dirname = path.resolve();
+app.use("/uploads", express.static(path.join(dirname, "/uploads")));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Back end is running on port: ${PORT}`)
-})
+  console.log(`Back end is running on port: ${PORT}`);
+});
