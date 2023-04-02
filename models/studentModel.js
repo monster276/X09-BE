985eecded7b687dbc683f10f86697f20d2450fe8
trunghieu <trunghieu0451@gmail.com>
2023-04-02 16:11:34 +0700

    add student rollCall model

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,11 +1,30 @@
 const mongoose = require("mongoose");
 
-const studentSchema = mongoose.Schema({
+const rollCallSchema = mongoose.Schema({
   classroom: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "Classroom",
   },
+  lesson: {
+    type: Date,
+    required: true,
+  },
+  presence: {
+    type: Number,
+    required: true,
+  },
+  score: {
+    type: Number,
+    required: false,
+  },
+  comment: {
+    type: String,
+    required: false,
+  },
+});
+
+const studentSchema = mongoose.Schema({
   fullName: {
     type: String,
     required: true,
@@ -20,6 +39,7 @@ const studentSchema = mongoose.Schema({
     required: true,
     unique: true,
   },
+  rollCall: [rollCallSchema],
 });
 
 const Student = mongoose.model("Student", studentSchema);
