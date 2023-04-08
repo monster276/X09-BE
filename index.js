8e1755b1bcd38ffe0733c11396c5ea3db2a4a603
trunghieu <trunghieu0451@gmail.com>
2023-04-08 17:10:53 +0700

    fix

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ const cors = require("cors");
 const express = require("express");
 const { connectToDB } = require("./config/db");
 
-<<<<<<< HEAD
-// Routes
-const authRoutes = require('./routes/auth')
-const courseRoutes = require('./routes/courseRoutes')
-const classroomRoutes = require('./routes/classroomRoutes')
-const uploadRoutes = require('./routes/uploadRoutes')
-const locationRoutes = require('./routes/locationRoutes')
-const lectureRoutes = require('./routes/lectureRoutes')
-const enrollCourseRoutes = require('./routes/enrollCourseRoutes')
-const userRoute = require("./routes/userRoute")
-=======
 const authRoutes = require("./routes/auth");
 const courseRoutes = require("./routes/courseRoutes");
 const classroomRoutes = require("./routes/classroomRoutes");
@@ -27,7 +16,6 @@ const lessonRoutes = require("./routes/lessonRoutes");
 const enrollCourseRoutes = require("./routes/enrollCourseRoutes");
 const userRoute = require("./routes/userRoute");
 
->>>>>>> efed287dd09c4fcb8f54dd6f85a36eff2e9748e2
 //Connect Mongo
 connectToDB();
 
@@ -39,18 +27,6 @@ app.use(express.json());
 app.get("/", (req, res) => res.json({ msg: "Welcome to the API" }));
 
 // Define Routes
-<<<<<<< HEAD
-app.use('/api/courses', courseRoutes)
-app.use('/api/classrooms', classroomRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoute)
-app.use('/api/upload', uploadRoutes)
-app.use('/api/locations', locationRoutes)
-app.use('/api/lectures', lectureRoutes)
-app.use('/api/enrollCourse', enrollCourseRoutes)
-const dirname = path.resolve()
-app.use('/uploads', express.static(path.join(dirname, '/uploads')))
-=======
 app.use("/api/courses", courseRoutes);
 app.use("/api/classrooms", classroomRoutes);
 app.use("/api/attendances", attendancesRoutes);
@@ -64,7 +40,6 @@ app.use("/api/lessons", lessonRoutes);
 app.use("/api/enrollCourse", enrollCourseRoutes);
 const dirname = path.resolve();
 app.use("/uploads", express.static(path.join(dirname, "/uploads")));
->>>>>>> efed287dd09c4fcb8f54dd6f85a36eff2e9748e2
 
 const PORT = process.env.PORT || 5000;
 
